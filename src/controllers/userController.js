eee1ecc5dbdffbe7d2cfa1006fb13a34a75e4ebc
sspark89 <sosn3@naver.com>
2025-03-18 09:25:14 +0900

    user api 호출 시 request 처리하는 방법

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,8 +3,28 @@ const pool = require("../config/db");
 // 유저 조회
 const getUsers = async (req, res) => {
   try {
-    const [rows] = await pool.query("SELECT * FROM users");
-    res.json(rows);
+    // const [rows] = await pool.query("SELECT * FROM users");
+    // res.json(rows);
+
+    const { name, email } = req.query;
+
+    console.log("Received request with name:", name, "email:", email);  // 디버그 로그
+    
+    let query = "SELECT * FROM users WHERE 1=1";
+    const values = [];
+
+    if (name) {
+      query += " AND name = ?";
+      values.push(name);
+    }
+    if (email) {
+      query += " AND email = ?";
+      values.push(email);
+    }
+
+    const [users] = await pool.query(query, values);
+    res.status(200).json(users);
+
   } catch (error) {
     res.status(500).json({ error: "서버 오류" });
   }
